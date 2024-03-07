abe46af670cd22f563063b21676c1c101a0b5fae
dagarh <hdagar3@gmail.com>
2024-03-06 19:26:11 -0500

    modification of service name

---
+++ Backend/user-registration-and-preference/src/main/java/com/ssw695/app/roommatefinderauth/RoommateFinderAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ssw695.app.roommatefinderauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoommateFinderAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoommateFinderAuthApplication.class, args);
+	}
+
+}
