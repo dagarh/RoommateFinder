4603e46272e0895d8740817b77704fc8c5d3f675
Kartheekbandi <123476304+Kartheekbandi@users.noreply.github.com>
2024-04-22 17:21:28 -0700

    Revert "IN app chatting"
    This reverts commit b6623ca68d3a920c6365978d5b3cb782b8d521ae.

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,8 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDLHroMkvDFuTYpZeEl_fWbnYbA8YvxmEU",
+//   authDomain: "roommate-b495b.firebaseapp.com",
+//   projectId: "roommate-b495b",
+//   storageBucket: "roommate-b495b.appspot.com",
+//   messagingSenderId: "797925756036",
+//   appId: "1:797925756036:web:03c5b6d57c09f1c1af06d1",
+// };
 const firebaseConfig = {
   apiKey: "AIzaSyDLHroMkvDFuTYpZeEl_fWbnYbA8YvxmEU",
   authDomain: "roommate-b495b.firebaseapp.com",
@@ -16,4 +23,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
 export const db = getFirestore(app);
-export const auth= getAuth()
+export const auth = getAuth();
+
--- screens/Chats.js
+++ screens/Chats.js
@@ -1,84 +1,13 @@
-import React, {
-  useState,
-  useEffect,
-  useLayoutEffect,
-  useCallback
-} from 'react';
-import { TouchableOpacity, Text } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
-import {
-  collection,
-  addDoc,
-  orderBy,
-  query,
-  onSnapshot
-} from 'firebase/firestore';
-import { signOut } from 'firebase/auth';
-import { useNavigation } from '@react-navigation/native';
-import { AntDesign } from '@expo/vector-icons';
-import { db,auth } from '../FirebaseConfig';
-import colors from '../constants/colors';
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import MyText from '../components/MyText'
 
-export default function Chats() {
+const Chats = () => {
+  return (
+    <SafeAreaView>
+      <MyText type='title'>Chats</MyText>
+    </SafeAreaView>
+  );
+};
 
-  const [messages, setMessages] = useState([]);
-  const navigation = useNavigation();
-
-  useLayoutEffect(() => {
-
-      const collectionRef = collection(db, 'Chats');
-      const q = query(collectionRef, orderBy('createdAt', 'desc'));
-
-  const unsubscribe = onSnapshot(q, querySnapshot => {
-      console.log('querySnapshot unsusbscribe');
-        setMessages(
-          querySnapshot.docs.map(doc => ({
-            _id: doc.data()._id,
-            createdAt: doc.data().createdAt.toDate(),
-            text: doc.data().text,
-            user: doc.data().user
-          }))
-        );
-      });
-  return unsubscribe;
-    }, []);
-
-  const onSend = useCallback((messages = []) => {
-      setMessages(previousMessages =>
-        GiftedChat.append(previousMessages, messages)
-      );
-      // setMessages([...messages, ...messages]);
-      const { _id, createdAt, text, user } = messages[0];    
-      addDoc(collection(db, 'Chats'), {
-        _id,
-        createdAt,
-        text,
-        user
-      });
-    }, []);
-
-    return (
-      // <>
-      //   {messages.map(message => (
-      //     <Text key={message._id}>{message.text}</Text>
-      //   ))}
-      // </>
-      <GiftedChat
-        messages={messages}
-        showAvatarForEveryMessage={false}
-        showUserAvatar={false}
-        onSend={messages => onSend(messages)}
-        messagesContainerStyle={{
-          backgroundColor: '#fff'
-        }}
-        textInputStyle={{
-          backgroundColor: '#fff',
-          borderRadius: 20,
-        }}
-        user={{
-          _id: auth?.currentUser?.email,
-          avatar: 'https://i.pravatar.cc/300'
-        }}
-      />
-    );
-}
+export default Chats;
