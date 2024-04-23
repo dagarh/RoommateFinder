2a972969919a6bdf58f7af6e9f8f7f77eaa7666f
Kartheekbandi <123476304+Kartheekbandi@users.noreply.github.com>
2024-04-22 17:51:51 -0700

    Revert "Revert "IN app chatting""
    This reverts commit 4603e46272e0895d8740817b77704fc8c5d3f675.

--- screens/Chats.js
+++ screens/Chats.js
@@ -1,13 +1,84 @@
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import MyText from '../components/MyText'
+import React, {
+  useState,
+  useEffect,
+  useLayoutEffect,
+  useCallback
+} from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import {
+  collection,
+  addDoc,
+  orderBy,
+  query,
+  onSnapshot
+} from 'firebase/firestore';
+import { signOut } from 'firebase/auth';
+import { useNavigation } from '@react-navigation/native';
+import { AntDesign } from '@expo/vector-icons';
+import { db,auth } from '../FirebaseConfig';
+import colors from '../constants/colors';
 
-const Chats = () => {
-  return (
-    <SafeAreaView>
-      <MyText type='title'>Chats</MyText>
-    </SafeAreaView>
-  );
-};
+export default function Chats() {
 
-export default Chats;
+  const [messages, setMessages] = useState([]);
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+
+      const collectionRef = collection(db, 'Chats');
+      const q = query(collectionRef, orderBy('createdAt', 'desc'));
+
+  const unsubscribe = onSnapshot(q, querySnapshot => {
+      console.log('querySnapshot unsusbscribe');
+        setMessages(
+          querySnapshot.docs.map(doc => ({
+            _id: doc.data()._id,
+            createdAt: doc.data().createdAt.toDate(),
+            text: doc.data().text,
+            user: doc.data().user
+          }))
+        );
+      });
+  return unsubscribe;
+    }, []);
+
+  const onSend = useCallback((messages = []) => {
+      setMessages(previousMessages =>
+        GiftedChat.append(previousMessages, messages)
+      );
+      // setMessages([...messages, ...messages]);
+      const { _id, createdAt, text, user } = messages[0];    
+      addDoc(collection(db, 'Chats'), {
+        _id,
+        createdAt,
+        text,
+        user
+      });
+    }, []);
+
+    return (
+      // <>
+      //   {messages.map(message => (
+      //     <Text key={message._id}>{message.text}</Text>
+      //   ))}
+      // </>
+      <GiftedChat
+        messages={messages}
+        showAvatarForEveryMessage={false}
+        showUserAvatar={false}
+        onSend={messages => onSend(messages)}
+        messagesContainerStyle={{
+          backgroundColor: '#fff'
+        }}
+        textInputStyle={{
+          backgroundColor: '#fff',
+          borderRadius: 20,
+        }}
+        user={{
+          _id: auth?.currentUser?.email,
+          avatar: 'https://i.pravatar.cc/300'
+        }}
+      />
+    );
+}
